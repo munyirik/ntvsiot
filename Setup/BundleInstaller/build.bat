da652e9d4ddb8dc8637cc5f829370ac697066f72
Munyiri Kamau <munyirik@microsoft.com>
2015-07-24 15:45:37 -0700

    Changing name of bundle installer

--- Setup/BundleInstaller/build.bat
+++ Setup/BundleInstaller/build.bat
@@ -8,12 +8,12 @@ if "%WIX%"=="" (
 )
 
 "%WIX%bin\candle.exe" NTVSBundleInstaller.wxs -ext WixBalExtension
-"%WIX%bin\light.exe" NTVSBundleInstaller.wixobj -ext WixBalExtension -cultures:en-us -loc Theme.wxl -out "NTVS Bundle Installer.exe"
+"%WIX%bin\light.exe" NTVSBundleInstaller.wixobj -ext WixBalExtension -cultures:en-us -loc Theme.wxl -out "NTVS IoT Installer.exe"
 
 echo Signing the bundle installer...
-"%WIX%bin\insignia.exe" -ib "NTVS Bundle Installer.exe" -o engine.exe
-powershell -command "& { . .\sign.ps1; begin_sign_files -files 'engine.exe' -bindir '%bundle_dir%' -outdir '%bundle_dir%SignedInstaller' -approvers 'jinglou','sitani' -projectName 'NTVS IoT' -projectUrl 'https://github.com/ms-iot/ntvsiot' -jobDescription 'NTVS Bundle Installer' -jobKeywords 'NTVS', 'Node.js' -certificates 'authenticode'}"
-"%WIX%bin\insignia.exe" -ab "%bundle_dir%SignedInstaller\engine.exe" "NTVS Bundle Installer.exe" -o "NTVS Bundle Installer.exe"
-powershell -command "& { . .\sign.ps1; begin_sign_files -files 'NTVS Bundle Installer.exe' -bindir '%bundle_dir%' -outdir '%bundle_dir%SignedInstaller' -approvers 'jinglou','sitani' -projectName 'NTVS IoT' -projectUrl 'https://github.com/ms-iot/ntvsiot' -jobDescription 'NTVS Bundle Installer' -jobKeywords 'NTVS', 'Node.js' -certificates 'authenticode'}"
+"%WIX%bin\insignia.exe" -ib "NTVS IoT Installer.exe" -o engine.exe
+powershell -command "& { . .\sign.ps1; begin_sign_files -files 'engine.exe' -bindir '%bundle_dir%' -outdir '%bundle_dir%SignedInstaller' -approvers 'jinglou','sitani' -projectName 'NTVS IoT' -projectUrl 'https://github.com/ms-iot/ntvsiot' -jobDescription 'NTVS IoT Wix Burn Engine' -jobKeywords 'NTVS', 'Node.js' -certificates 'authenticode'}"
+"%WIX%bin\insignia.exe" -ab "%bundle_dir%SignedInstaller\engine.exe" "NTVS IoT Installer.exe" -o "NTVS IoT Installer.exe"
+powershell -command "& { . .\sign.ps1; begin_sign_files -files 'NTVS IoT Installer.exe' -bindir '%bundle_dir%' -outdir '%bundle_dir%SignedInstaller' -approvers 'jinglou','sitani' -projectName 'NTVS IoT' -projectUrl 'https://github.com/ms-iot/ntvsiot' -jobDescription 'NTVS IoT' -jobKeywords 'NTVS', 'Node.js' -certificates 'authenticode'}"
 
 :end
