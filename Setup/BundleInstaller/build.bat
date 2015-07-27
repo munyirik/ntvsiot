b8f486a18e673e247b85ca64242da6905b3d7078
Munyiri Kamau <munyirik@microsoft.com>
2015-07-27 14:46:31 -0700

    Changing name of installer

--- Setup/BundleInstaller/build.bat
+++ Setup/BundleInstaller/build.bat
@@ -8,12 +8,12 @@ if "%WIX%"=="" (
 )
 
 "%WIX%bin\candle.exe" NTVSBundleInstaller.wxs -ext WixBalExtension
-"%WIX%bin\light.exe" NTVSBundleInstaller.wixobj -ext WixBalExtension -cultures:en-us -loc Theme.wxl -out "NTVS IoT Installer.exe"
+"%WIX%bin\light.exe" NTVSBundleInstaller.wixobj -ext WixBalExtension -cultures:en-us -loc Theme.wxl -out "Node.js Tools for Windows IoT 1.1.exe"
 
 echo Signing the bundle installer...
-"%WIX%bin\insignia.exe" -ib "NTVS IoT Installer.exe" -o engine.exe
+"%WIX%bin\insignia.exe" -ib "Node.js Tools for Windows IoT 1.1.exe" -o engine.exe
 powershell -command "& { . .\sign.ps1; begin_sign_files -files 'engine.exe' -bindir '%bundle_dir%' -outdir '%bundle_dir%SignedInstaller' -approvers 'jinglou','sitani' -projectName 'Node.js Tools for Windows IoT' -projectUrl 'https://github.com/ms-iot/ntvsiot' -jobDescription 'NTVS IoT Wix Burn Engine' -jobKeywords 'NTVS', 'Node.js' -certificates 'authenticode'}"
-"%WIX%bin\insignia.exe" -ab "%bundle_dir%SignedInstaller\engine.exe" "NTVS IoT Installer.exe" -o "NTVS IoT Installer.exe"
-powershell -command "& { . .\sign.ps1; begin_sign_files -files 'NTVS IoT Installer.exe' -bindir '%bundle_dir%' -outdir '%bundle_dir%SignedInstaller' -approvers 'jinglou','sitani' -projectName 'Node.js Tools for Windows IoT' -projectUrl 'https://github.com/ms-iot/ntvsiot' -jobDescription 'Node.js Tools for Windows IoT' -jobKeywords 'NTVS', 'Node.js' -certificates 'authenticode'}"
+"%WIX%bin\insignia.exe" -ab "%bundle_dir%SignedInstaller\engine.exe" "Node.js Tools for Windows IoT 1.1.exe" -o "Node.js Tools for Windows IoT 1.1.exe"
+powershell -command "& { . .\sign.ps1; begin_sign_files -files 'Node.js Tools for Windows IoT 1.1.exe' -bindir '%bundle_dir%' -outdir '%bundle_dir%SignedInstaller' -approvers 'jinglou','sitani' -projectName 'Node.js Tools for Windows IoT' -projectUrl 'https://github.com/ms-iot/ntvsiot' -jobDescription 'Node.js Tools for Windows IoT' -jobKeywords 'NTVS', 'Node.js' -certificates 'authenticode'}"
 
 :end
