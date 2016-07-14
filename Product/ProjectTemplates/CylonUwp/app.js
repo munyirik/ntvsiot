a1c682808b871126f898ec67dd69194ae06ae302
Munyiri Kamau <munyirik@microsoft.com>
2016-07-14 14:03:40 -0700

    Update template based on serialport changes

--- Product/ProjectTemplates/CylonUwp/app.js
+++ Product/ProjectTemplates/CylonUwp/app.js
@@ -6,15 +6,9 @@ var Cylon = require('cylon');
 
 Cylon.robot({
     connections: {
-        // The port name used in connections can either be a regular port name like
-        // "COM1" or it can be in a format describing a USB-Serial device that has
-        // not been assigned a port name. The format is:
-        // USB[#|\]VID_<Vendor ID>&PID_<Product ID>[#|\]<Serial String>
-        // Example:
-        // USB#VID_2341&PID_0043#85436323631351311141
-        // This string can be found by either:
-        // 1. Calling the serialport list function.
-        // 2. Running "devcon status usb*" on your Windows 10 IoT Core device (only for USB-Serial devices).
+        // Use the serialport list function to get the port value for your connection. 
+        // If a device ID is provided as the comName, be sure to double the backslashes 
+        // in the string. e.g. \\?\ should be \\\\?\\
     },
 
     devices: {
