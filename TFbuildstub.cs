974ab7102c98a5f930e2b4e6f95bee83fa0d4409
Munyiri <munyirik@microsoft.com>
2016-07-28 15:28:59 -0700

    Add Team Foundation files

---
+++ TFbuildstub.cs
@@ -0,0 +1,9 @@
+// This stub is used in Team Foundation project files to successfully build releases
+
+using System;
+
+class HelloWorld {
+    static void Main() {
+        Console.WriteLine("buildstub: no-op");
+    }
+}
